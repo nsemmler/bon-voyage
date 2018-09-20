dba9b303f151d5931a928b36295fd894b5d8c94a
Nate <ndsunc@gmail.com>
2018-09-20 10:40:24 -0700

    Commented in login and signup pending dispatch actions and reducers for slight pause before logging in/signing up

--- src/actions/auth.actions.js
+++ src/actions/auth.actions.js
@@ -1,22 +1,23 @@
 import axios from 'axios'
 
-// export const USER_LOGIN_PENDING = "USER_LOGIN_PENDING"
+export const USER_LOGIN_PENDING = "USER_LOGIN_PENDING"
 export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS"
 export const USER_LOGIN_FAILED = "USER_LOGIN_FAILED"
-// export const USER_SIGNUP_PENDING = "USER_SIGNUP_PENDING"
+export const USER_SIGNUP_PENDING = "USER_SIGNUP_PENDING"
 export const USER_SIGNUP_SUCCESS = "USER_SIGNUP_SUCCESS"
 export const USER_SIGNUP_FAILED = "USER_SIGNUP_FAILED"
 export const USER_LOGOUT = "USER_LOGOUT"
 
 const BASE_URL = "http://localhost:3000"
 
+window.axios = axios
+
 export const userLogin = ({ email, password }) => {
   return async (dispatch) => {
     try {
-      // dispatch({ type: USER_LOGIN_PENDING })
-
-      let response = await axios.post(`${BASE_URL}/users/login`, { email, password })
-      dispatch({ type: USER_LOGIN_SUCCESS, payload: response.data })
+      dispatch({ type: USER_LOGIN_PENDING })
+      let response = await axios.post(`${BASE_URL}/login`, { 'user': { 'email': email, 'password': password } })
+      dispatch({ type: USER_LOGIN_SUCCESS, payload: response })
     } catch (err) {
       dispatch({ type: USER_LOGIN_FAILED, payload: err })
     }
@@ -24,15 +25,18 @@ export const userLogin = ({ email, password }) => {
 }
 
 export const userSignup = (newUser) => {
+  console.log('userSignup newUser: ', newUser)
   return async (dispatch) => {
     try {
-      // dispatch({ type: USER_LOGIN_PENDING })
+      dispatch({ type: USER_LOGIN_PENDING })
 
-      let response = await fetch(`${BASE_URL}/users`, {
+      let response = await fetch(`${BASE_URL}/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newUser)
       })
+
+      console.log('userSignup response: ', response)
 
       dispatch({ type: USER_SIGNUP_SUCCESS, payload: response.data })
     } catch (err) {
--- src/reducers/auth.reducers.js
+++ src/reducers/auth.reducers.js
@@ -1,29 +1,30 @@
 import {
-  // USER_LOGIN_PENDING,
+  USER_LOGIN_PENDING,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAILED,
-  // USER_SIGNUP_PENDING,
+  USER_SIGNUP_PENDING,
   USER_SIGNUP_SUCCESS,
   USER_SIGNUP_FAILED
 } from "../actions/auth.actions"
 
 let initialState = {
   isLoading: false,
-  user: {},
   showLoginError: false,
-  showSignupError: false
+  showSignupError: false,
+  user: {},
+  token: ''
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    // case USER_LOGIN_PENDING:
-    //   return { ...state, isLoading: true }
+    case USER_LOGIN_PENDING:
+      return { ...state, isLoading: true }
     case USER_LOGIN_SUCCESS:
-      return { ...state, isLoading: false, user: action.payload }
+      return { ...state, isLoading: false, user: action.payload.data, token: action.payload.headers.authorization }
     case USER_LOGIN_FAILED:
       return { ...state, isLoading: false, showLoginError: true }
-    // case USER_SIGNUP_PENDING:
-    //   return { ...state, isLoading: true }
+    case USER_SIGNUP_PENDING:
+      return { ...state, isLoading: true }
     case USER_SIGNUP_SUCCESS:
       return { ...state, isLoading: false }
     case USER_SIGNUP_FAILED:
