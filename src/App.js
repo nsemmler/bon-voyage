172cca7fe8f9924024d078283aa013f87eaf9122
Nate <ndsunc@gmail.com>
2018-09-24 15:00:01 -0700

    Generating <Main> with CSS flexbox, grid to better display the first question

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react'
 import LoginForm from './components/LoginForm'
 import SignupForm from './components/SignupForm'
-import UserProfile from './components/UserProfile'
+import Main from './components/Main'
 import Nav from './components/Nav'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
-export const App = () => {
+export default function App () {
   return (
-    <div>
+    <div className="main">
       <Nav />
       <Router>
         <Switch>
           <Route exact path="/login" component={ LoginForm } />
           <Route exact path="/signup" component={ SignupForm } />
-          <Route exact path="/profile" component={ UserProfile } />
+          <Main />
           <Redirect to="/login" />
         </Switch>
       </Router>
     </div>
   )
 }
-
-export default App
