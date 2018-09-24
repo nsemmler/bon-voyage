e4f3f984a0a668a9bbbbe1e2b78d503fbdcf58ee
Nate <ndsunc@gmail.com>
2018-09-24 14:56:41 -0700

    store formed with root reducers

--- src/store.js
+++ src/store.js
@@ -6,3 +6,17 @@ import thunk from 'redux-thunk'
 export default (initialState) => {
   return createStore(rootReducer, applyMiddleware(thunk, logger))
 }
+
+
+
+
+// store = {
+//   isLoading: false,
+//   showLoginError: false,
+//   showSignupError: false,
+//   user: {},
+//   curr_question_id: 0,
+//   curr_question: "",
+//   curr_question_options: [],
+//   users_answers: {}
+// }
