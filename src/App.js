f1d277790ba6d1aad6b10d5bed4ef3bfc0501c0a
Nate <ndsunc@gmail.com>
2018-09-25 11:57:59 -0700

    Removed <Nav /> and <div> wrapping all of the components and added them to each individual component that renders the Nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react'
-import LoginForm from './components/LoginForm'
-import SignupForm from './components/SignupForm'
+import Login from './components/Login'
+import Signup from './components/Signup'
 import Main from './components/Main'
-import Nav from './components/Nav'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 export default function App () {
   return (
-    <div className="main">
-      <Nav />
-      <Router>
-        <Switch>
-          <Route exact path="/login" component={ LoginForm } />
-          <Route exact path="/signup" component={ SignupForm } />
-          <Main />
-          <Redirect to="/login" />
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/login" component={ Login } />
+        <Route exact path="/signup" component={ Signup } />
+        <Main />
+        <Redirect to="/login" />
+      </Switch>
+    </Router>
   )
 }
