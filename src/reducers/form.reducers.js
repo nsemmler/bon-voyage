4f9792f18c5410d5964da21b5730307d6cc78e1f
Nate <ndsunc@gmail.com>
2018-09-27 10:53:34 -0700

    Action UPDATE_ANSWER_CHOICE toggles the checked value for the given answer choice; SUBMIT_FORM submits the form and returns recommendations

--- src/reducers/form.reducers.js
+++ src/reducers/form.reducers.js
@@ -1,29 +1,35 @@
 import {
-  CHECK_REGION,
+  UPDATE_ANSWER_CHOICE,
   SUBMIT_FORM
 } from "../actions/form.actions"
 
 import initialState from '../questions.json'
 
-// separate initialState into object w/ questions={.json} & answers=[]
-// check answers.length in <Main>
 export default (state = initialState, action) => {
   switch (action.type) {
-    case CHECK_REGION:
-      console.log('Inside Reducer when click Region');
-      const id = action.payload
-      const regionQuestion = state[0]
+    case UPDATE_ANSWER_CHOICE:
+      var stateCopy = [ ...state ]
+      const answerChoiceID = action.payload.answerChoiceID
+      const questionID = action.payload.questionID
 
-      const updatedAnswerChoices = regionQuestion.answer_choices.map(choice => {
-        if (choice.id === id) {
-          return { ...choice, checked: !choice.checked }
+      const updatedStateCopy = stateCopy.map((question, i) => {
+        if (i === questionID) {
+          var updatedAnswerChoices = question.answer_choices.map(ansr_choice => {
+            if (ansr_choice.id === answerChoiceID) {
+              return { ...ansr_choice, checked: !ansr_choice.checked }
+            } else {
+              return ansr_choice
+            }
+          })
+          return { ...question, answer_choices: updatedAnswerChoices }
         } else {
-          return choice
+          return question
         }
       })
 
-      return [ { ...regionQuestion, answer_choices: updatedAnswerChoices } ]
+      return updatedStateCopy
     case SUBMIT_FORM:
+      console.log('Inside SUBMIT_FORM Reducer')
       return { recommendations: action.payload }
     default:
       return state
