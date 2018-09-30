0c4353004eda2b95eef02cec7e097b777dfdda5a
Nate <ndsunc@gmail.com>
2018-09-30 11:07:59 -0700

    Created component to display additional images of the country beyond the thumbnail

---
+++ src/components/CountryImages.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function CountryImages (props) {
+  return (
+    <div className="images-container">
+      {
+        JSON.parse(props.country.images).map((img, i) => {
+          return <img src={ img } alt={ `${props.country.name} Image ${i+1}` } className="countryimg" key={ `${props.country.name}-img-${props.country.id}` } />
+        })
+      }
+    </div>
+  )
+}
+
+CountryImages.propTypes = {
+  country: PropTypes.object.isRequired
+}
+
+export default CountryImages
