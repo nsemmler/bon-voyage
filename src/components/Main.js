6ea376e6d9b5c50f0917aca21bdf83dc4008359c
Nate <ndsunc@gmail.com>
2018-10-01 11:08:10 -0700

    Added SelectedAnswersChips to <Main> when showing recommended vacation destinations; returnToQuiz() and retakeQuiz() attached to buttons that display just above the Chips; Updated Previous, Next, Submit buttons that render at the bottom of the form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { selectAnswerChoice, submitUserQuiz } from '../actions/form.actions'
+import { selectAnswerChoice, submitUserQuiz, updateQuizAnswers, retakeQuiz } from '../actions/form.actions'
 import { Input, Button, Preloader } from 'react-materialize'
 import Question from './Question'
 import QuestionCount from './QuestionCount'
@@ -10,6 +10,7 @@ import Modal from 'react-modal'
 import Nav from './Nav'
 import CountryInfo from './CountryInfo'
 import Recommendations from './Recommendations'
+import SelectedAnswersChips from './SelectedAnswersChips'
 
 class Main extends Component {
   constructor(props) {
@@ -50,7 +51,20 @@ class Main extends Component {
 
   returnToQuiz = (e) => {
     e.preventDefault()
-    console.log('Inside returnToQuiz')
+
+    this.props.updateQuizAnswers()
+  }
+
+  retakeQuiz = (e) => {
+    e.preventDefault()
+
+    this.setState({
+      questionIndex: 0,
+      questionNum: this.props.form.questions.questions[0].id,
+      questionName: this.props.form.questions.questions[0].name,
+    })
+
+    this.props.retakeQuiz()
   }
 
   displayRecommendations = () => {
@@ -72,12 +86,26 @@ class Main extends Component {
   }
 
   displayTravelRecommendations = () => {
+    let chipsArr = this.props.form.questions.questions.map((question, i) => {
+      const selectedAnswers = question.answer_choices.filter((ansr_choice) => {
+        if (ansr_choice.checked) return ansr_choice.content
+      })
+
+      const selectedAnswerContent = selectedAnswers.map(ansr_obj => {
+        return ansr_obj.content
+      })
+
+      return selectedAnswerContent
+    })
+
     return (
       <div className="Main">
         <div className="main-header">
           <h5>Recommended Travel Destinations:</h5>
           <button className="back2quiz" onClick={ this.returnToQuiz } waves="light" type="button">Return to Quiz</button>
+          <button className="retakeQuiz" onClick={ this.retakeQuiz } waves="light" type="button">Retake Quiz</button>
         </div>
+        <SelectedAnswersChips chipsArr={ chipsArr }/>
         <br/>
         <div className="response-container" id="responses">
           { this.displayRecommendations() }
@@ -109,6 +137,8 @@ class Main extends Component {
   }
 
   displayTravelQuiz = () => {
+    let uniqueAnswerChoices = this.props.form.questions[this.state.questionIndex].answer_choices.map((ansr_choice) => { return ansr_choice.checked }).filter((val, i, array) => array.indexOf(val) === i)
+
     return (
       <div className="Main">
         <div className="main-header">
@@ -130,24 +160,25 @@ class Main extends Component {
                 <div className="inputs-container">
                   {
                     this.props.form.questions[this.state.questionIndex].answer_choices.map(ansr_choice => {
-                      console.log('ansr_choice', ansr_choice)
                       return <Input className="form-input" key={ `${ansr_choice.id}-${ansr_choice.checked}` } onClick={ (e) => this.toggleAnswerChoice(e, ansr_choice.id) } name={ `${ansr_choice.type}[]` } type='checkbox' checked={ ansr_choice.checked } label={ ansr_choice.content } />
                     })
                   }
                 </div>
 
-                {
-                  this.state.questionNum !== this.state.numQuestions && <div className="prev-next-questions-btns">
+                <div className={(this.state.questionNum !== this.state.numQuestions) ? "prev-next-questions-btns" : "submit-question-btn"}>
+                  {
+                    (this.state.questionNum !== 1) &&
                     <Button className="prevbtn" onClick={ this.fetchPreviousQuestion } waves="light" type="button" disabled={ this.state.questionNum === 1 }>Previous</Button>
-                    <Button className="nextbtn" onClick={ this.fetchNextQuestion } waves="light" type="button">Next</Button>
-                  </div>
-                }
-                {
-                  this.state.questionNum === this.state.numQuestions && <div className="submit-question-btn">
-                    <Button className="prevbtn" onClick={ this.fetchPreviousQuestion } waves="light" type="button">Previous</Button>
-                    <Button waves="light" type="submit">Submit</Button>
-                  </div>
-                }
+                  }
+                  {
+                    (this.state.questionNum !== this.state.numQuestions) &&
+                    <Button className="nextbtn" onClick={ this.fetchNextQuestion } waves="light" type="button" disabled={ (uniqueAnswerChoices.length === 1 && !uniqueAnswerChoices[0]) }>Next</Button>
+                  }
+                  {
+                    (this.state.questionNum === this.state.numQuestions) &&
+                    <Button waves="light" type="submit" disabled={ (uniqueAnswerChoices.length === 1 && !uniqueAnswerChoices[0]) }>Submit</Button>
+                  }
+                </div>
               </div>
             </form>
           </div>
@@ -176,7 +207,7 @@ function mapStateToProps (state) {
 
 function mapDispatchToProps (dispatch) {
   return bindActionCreators({
-    selectAnswerChoice, submitUserQuiz
+    selectAnswerChoice, submitUserQuiz, updateQuizAnswers, retakeQuiz
   }, dispatch)
 }
 
