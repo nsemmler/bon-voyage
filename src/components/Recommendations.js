cf754c8c08c4a565d85aec4c2cb1e8d84ab59b78
Nate <ndsunc@gmail.com>
2018-10-03 10:02:16 -0700

    displayCountryInformationModal() now passes countryIndex as additional param

--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -4,12 +4,12 @@ import { Card, CardTitle } from 'react-materialize'
 
 function Recommendations (props) {
   return (
-    props.recommendationsArr.map(country => {
+    props.recommendationsArr.map((country, countryIndex) => {
       if (country.name.toLowerCase().startsWith(props.countryName.toLowerCase()) || country.name.toLowerCase().includes(props.countryName.toLowerCase())) {
         return <Card className="recommendedCountry" key={ `recommendation-${country.id}` }
           header={ <CardTitle image={ JSON.parse(country.images)[0] }/> }
           title={ ((country.name === country.native_name) || (country.name === 'United States of America')) ? `${country.name}` : `${country.name} (${country.native_name})` }
-          onClick={ () => props.displayCountryInformationModal(country) }>
+          onClick={ () => props.displayCountryInformationModal(country, countryIndex) }>
           { country.capital }
         </Card>
       }
