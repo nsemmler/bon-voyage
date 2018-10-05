130de2a3d5b0f56e05d0c7c0a0a7148ed3d910d4
Nate <ndsunc@gmail.com>
2018-10-05 15:02:23 -0700

    GET_USER_FAVORITES updates favorites array; added favorites array to other calls

--- src/reducers/form.reducers.js
+++ src/reducers/form.reducers.js
@@ -3,15 +3,18 @@ import {
   UPDATE_QUIZ_ANSWERS,
   FORM_SUBMISSION_PENDING,
   RETAKE_QUIZ,
-  SUBMIT_FORM
+  SUBMIT_FORM,
+  GET_USER_FAVORITES
 } from "../actions/form.actions"
 
 import initialState from '../questions.json'
 
-export default (state = { questions: initialState, recommendations: [], pois: [], isLoading: false }, action) => {
+export default (state = { questions: initialState, recommendations: [], pois: [], favorites: [], isLoading: false }, action) => {
   switch (action.type) {
+    case GET_USER_FAVORITES:
+      return { ...state, favorites: action.payload }
     case FORM_SUBMISSION_PENDING:
-      return { ...state, recommendations: [], pois: [], isLoading: true }
+      return { ...state, recommendations: [], pois: [], favorites: [], isLoading: true }
     case UPDATE_ANSWER_CHOICE:
       var questionsCopy = [ ...state.questions ]
       const answerChoiceID = action.payload.answerChoiceID
@@ -40,13 +43,13 @@ export default (state = { questions: initialState, recommendations: [], pois: []
         }
       })
 
-      return { questions: updatedQuestionsCopy, recommendations: [], pois: [], isLoading: false }
+      return { questions: updatedQuestionsCopy, recommendations: [], pois: [], favorites: [], isLoading: false }
     case UPDATE_QUIZ_ANSWERS:
-      return { questions: state.questions, recommendations: [], pois: [], isLoading: false }
+      return { questions: state.questions, recommendations: [], pois: [], favorites: [], isLoading: false }
     case RETAKE_QUIZ:
-      return { questions: initialState, recommendations: [], pois: [], isLoading: false }
+      return { questions: initialState, recommendations: [], pois: [], favorites: [], isLoading: false }
     case SUBMIT_FORM:
-      return { ...state, recommendations: action.payload.countries, pois: action.payload.pois, isLoading: false }
+      return { ...state, recommendations: action.payload.countries, pois: action.payload.pois, favorites: [], isLoading: false }
     default:
       return state
   }
