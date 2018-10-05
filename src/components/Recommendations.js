3489724399a57054e6bc94203123a46a652b8a56
Nate <ndsunc@gmail.com>
2018-10-05 15:01:30 -0700

    Added Material Icon to every card - hidden for now

--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Card, CardTitle } from 'react-materialize'
+import MaterialIcon from 'material-icons-react'
 
 function Recommendations (props) {
   return (
     props.recommendationsArr.map((country, countryIndex) => {
       if (country.name.toLowerCase().startsWith(props.countryName.toLowerCase()) || country.name.toLowerCase().includes(props.countryName.toLowerCase())) {
         return <Card className="recommendedCountry" key={ `recommendation-${country.id}` }
-          header={ <CardTitle image={ JSON.parse(country.images)[0] }/> }
-          title={ ((country.name === country.native_name) || (country.name === 'United States of America')) ? `${country.name}` : `${country.name} (${country.native_name})` }
+          header={ <CardTitle image={ JSON.parse(country.images)[0] } /> }
+          title={ `${country.name}` }
           onClick={ () => props.displayCountryInformationModal(country, countryIndex) }>
-          { country.capital }
+          { country.capital }{ false && <MaterialIcon icon="favorite" size="medium" color="#d10808"/>}
         </Card>
       }
     })
