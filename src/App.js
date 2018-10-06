00d5f91c3c9fde23179028eebf66d26c843df52f
Nate <ndsunc@gmail.com>
2018-10-06 12:41:39 -0700

    <Nav> rendered in App.js now; <Main> renders as a private route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Main from './components/Main'
+import Nav from './components/Nav'
 import PrivateRoute from './components/PrivateRoute'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
@@ -11,11 +12,12 @@ require('dotenv').config()
 export default function App () {
   return (
     <div id="main">
+      <Nav />
       <Router>
         <Switch>
           <Route exact path="/login" component={ Login } />
           <Route exact path="/signup" component={ Signup } />
-          <PrivateRoute path="/quiz" component={ Main } />
+          <PrivateRoute exact path="/" component={ Main } />
           <Redirect to="/login" />
         </Switch>
       </Router>
