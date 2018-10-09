cb489d903afd982f12424ea536f7368e08097efe
Nate <ndsunc@gmail.com>
2018-10-09 10:02:14 -0700

    Updated styling for Favorites to mimic Recommendations

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -3,15 +3,17 @@ import PropTypes from 'prop-types'
 import { Card, CardTitle } from 'react-materialize'
 import MaterialIcon from 'material-icons-react'
 import Modal from 'react-modal'
+import CountryInfo from './CountryInfo'
+import '../styling/Favorites.css'
 
 function Favorites (props) {
   // add a generic thingy that prompts a user to add favorites by taking the quiz (if none)
   return (
-    <div className="favorites-container">
-      <div className="Favorites-header">
-        <h5>Favorites:</h5>
+    <div className="Favorites">
+      <div className="favorites-header-container">
+        <h5 className="favorites-header">Favorites:</h5>
         <div className="filter-wrapper">
-          <span><input onChange={ (e) => props.filterFavorites(e) } id="filter" placeholder="Filter Favorites" /><i className="material-icons">search</i></span>
+          <input onChange={ (e) => props.filterFavorites(e) } id="filter" placeholder="Filter Favorites" autoFocus/>
         </div>
       </div>
       <br/>
@@ -20,17 +22,16 @@ function Favorites (props) {
           <Modal isOpen={ props.showCountryInfo } contentLabel="Recommended Country Information" onRequestClose={ () => props.displayCountryInformationModal({}) } shouldCloseOnOverlayClick={ true }>
             <button className="favoritebtn" onClick={ () => props.renameThisFnLater(props.selectedCountry, props.selectedCountryId) }><MaterialIcon icon="favorite" size="medium" color="#d10808"/></button>
             <div className="modal-container">
-              <p>PLACEHOLDER FOR COUNTRY INFO</p>
+              { (Object.keys(props.selectedCountry).length !== 0) && <CountryInfo country={ props.selectedCountry } countryIndex={ props.selectedCountryId } pointsOfInterest={ props.favorites.pois } /> }
             </div>
           </Modal>
           {
             props.favorites.countries.map((country, countryIndex) => {
               if (country.name.toLowerCase().startsWith(props.countryName.toLowerCase()) || country.name.toLowerCase().includes(props.countryName.toLowerCase())) {
                 return <Card className="favoriteCountry" key={ `favorite-${country.id}` }
-                  header={ <CardTitle image={ JSON.parse(country.images)[0] } /> }
                   title={ `${country.name}` }
+                  header={ <CardTitle className="favoriteCountry-img" image={ JSON.parse(country.images)[0] } /> }
                   onClick={ () => props.displayCountryInformationModal(country, countryIndex) }>
-                  { country.capital }{ false && <MaterialIcon icon="favorite" size="medium" color="#d10808"/>}
                 </Card>
               }
             })
@@ -50,6 +51,7 @@ Favorites.propTypes = {
   renameThisFnLater: PropTypes.func.isRequired,
   selectedCountry: PropTypes.object.isRequired,
   selectedCountryId: PropTypes.number.isRequired,
+  pois: PropTypes.array.isRequired
 }
 
 export default Favorites
