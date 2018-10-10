9b8095e913c70a71634374ac3252e6057ef80904
Nate <ndsunc@gmail.com>
2018-10-10 10:35:01 -0700

    Removed a lot of data redundant w/ map rendering next to this component in CountryInfo

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import '../styling/CountryInfo.css'
 
 function GeneralInfo (props) {
   return (
     <div className="general-info-container">
-      <ul className="info-items">
-        <li className="countryInfoItem">Region: { props.country.region }</li>
-        <li className="countryInfoItem">Subregion: { props.country.subregion }</li>
-        <li className="countryInfoItem">Population: { props.country.population }</li>
-        <li className="countryInfoItem">Lon/Lat: { props.country.longitude }/{ props.country.latitude }</li>
-        <li className="countryInfoItem">Currency: { props.country.currency_name })({ props.country.currency_symbol })</li>
-        <li className="countryInfoItem">Languages: { props.country.languages }</li>
-        <li className="countryInfoItem">Flag:</li>
-        <li className="countryInfoItem">{ <img src={ props.country.flag } alt={ `${props.country.name} Flag` } /> }</li>
-      </ul>
+      <div className="info-div">
+        <h4 className="country-name">{ props.country.name }</h4>
+        <h5 className="country-capital">{ props.country.capital }</h5>
+        <ul className="info-items">
+          <li className="countryInfoItem">Population: { props.country.population }</li>
+          <li className="countryInfoItem">Currency: { props.country.currency_name } ({ props.country.currency_symbol })</li>
+          <li className="countryInfoItem">Languages: { JSON.parse(props.country.languages) }</li>
+        </ul>
+      </div>
+      <div className="flag-div">
+        <img src={ props.country.flag } alt={ `${props.country.name} Flag` } className="countryflag" />
+      </div>
     </div>
   )
 }
