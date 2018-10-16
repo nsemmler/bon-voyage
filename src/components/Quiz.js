2ff8461bb4de8ef853c956d5551952263cc2f62e
Nate <ndsunc@gmail.com>
2018-10-16 13:33:28 -0700

    Divided <Main> and separated state, functions among its components

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,53 +1,105 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { withRouter } from 'react-router-dom'
 import Question from './Question'
 import QuestionCount from './QuestionCount'
 import QuizForm from './QuizForm'
+import { selectAnswerChoice, submitUserQuiz, updateQuizAnswers, retakeQuiz, fetchUserFavorites, removeFromFavorites, addToFavorites } from '../actions/form.actions'
 import '../styling/Quiz.css'
 
-function Quiz (props) {
-  return (
-    <div className="Quiz">
-      <div className="quiz-header-container">
-        <h5 className="quiz-header">Travel Quiz:</h5>
-      </div>
-      <br/>
-      <div className="quiz-container">
-        <div className="question-and-question-count-container">
-          <div className="question-container">
-            <Question content={ props.form.questions[props.questionIndex].question } />
-          </div>
-          <div className="question-counter-container">
-            <QuestionCount counter={ props.questionNum } total={ props.numQuestions } />
-          </div>
+class Quiz extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      questionIndex: 0,
+      questionNum: this.props.questions[0].id,
+      numQuestions: this.props.questions.length,
+      showCountryInfo: false,
+      selectedCountry: {},
+      selectedCountryId: 0,
+      countryName: ""
+    }
+  }
+
+  submitQuiz = async (e) => {
+    e.preventDefault()
+    var quiz = this.props.form.questions
+    await this.props.submitUserQuiz(quiz)
+    if (this.props.form.recommendations.length) this.props.history.push("/recommendations")
+  }
+
+  toggleAnswerChoice = (e, id) => {
+    e.preventDefault()
+    this.props.selectAnswerChoice(this.state.questionIndex, id)
+  }
+
+  fetchPreviousQuestion = (e) => {
+    e.preventDefault()
+
+    if (this.state.questionIndex > 0) {
+      this.setState({
+        questionIndex: --this.state.questionIndex,
+        questionNum: --this.state.questionNum
+      })
+    }
+  }
+
+  fetchNextQuestion = (e) => {
+    e.preventDefault()
+
+    if (this.state.questionNum < this.state.numQuestions) {
+      this.setState({
+        questionIndex: ++this.state.questionIndex,
+        questionNum: ++this.state.questionNum
+      })
+    }
+  }
+
+  render () {
+    const uniqueAnswerChoices = this.props.form.questions[this.state.questionIndex].answer_choices.map((ansr_choice) => { return ansr_choice.checked }).filter((val, i, array) => array.indexOf(val) === i)
+
+    return (
+      <div className="Quiz">
+        <div className="quiz-header-container">
+          <h5 className="quiz-header">Travel Quiz:</h5>
         </div>
-        <QuizForm
-          submitQuiz={ props.submitQuiz }
-          uniqueAnswerChoices={ props.uniqueAnswerChoices }
-          form={ props.form }
-          questionIndex={ props.questionIndex }
-          questionNum={ props.questionNum }
-          numQuestions={ props.numQuestions }
-          fetchPreviousQuestion={ props.fetchPreviousQuestion }
-          fetchNextQuestion={ props.fetchNextQuestion }
-          toggleAnswerChoice={ props.toggleAnswerChoice }
-        />
+        <br/>
+        <div className="quiz-container">
+          <div className="question-and-question-count-container">
+            <div className="question-container">
+              <Question content={ this.props.questions[this.state.questionIndex].question } />
+            </div>
+            <div className="question-counter-container">
+              <QuestionCount counter={ this.state.questionNum } total={ this.state.numQuestions } />
+            </div>
+          </div>
+          <QuizForm
+            submitQuiz={ this.submitQuiz }
+            uniqueAnswerChoices={ uniqueAnswerChoices }
+            form={ this.props.form }
+            questionIndex={ this.state.questionIndex }
+            questionNum={ this.state.questionNum }
+            numQuestions={ this.state.numQuestions }
+            fetchPreviousQuestion={ this.fetchPreviousQuestion }
+            fetchNextQuestion={ this.fetchNextQuestion }
+            toggleAnswerChoice={ this.toggleAnswerChoice }
+          />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-Quiz.propTypes = {
-  form: PropTypes.object.isRequired,
-  questionIndex: PropTypes.number.isRequired,
-  questionNum: PropTypes.number.isRequired,
-  numQuestions: PropTypes.number.isRequired,
-  uniqueAnswerChoices: PropTypes.array.isRequired,
-  submitQuiz: PropTypes.func.isRequired,
-  fetchPreviousQuestion: PropTypes.func.isRequired,
-  fetchNextQuestion: PropTypes.func.isRequired,
-  toggleAnswerChoice: PropTypes.func.isRequired,
-  goToFavorites: PropTypes.func.isRequired
+
+function mapStateToProps (state) {
+  return { questions: state.form.questions, form: state.form }
 }
 
-export default Quiz
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators({
+    selectAnswerChoice, submitUserQuiz, updateQuizAnswers, retakeQuiz, fetchUserFavorites, removeFromFavorites, addToFavorites
+  }, dispatch)
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Quiz))
