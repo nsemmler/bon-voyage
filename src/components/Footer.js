c6a9f9133700ff916414bac00ed1e2b6dd96405d
Nate <ndsunc@gmail.com>
2018-10-19 14:52:52 -0700

    Updated footer links to open in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ function Footer () {
     <div className="footer">
       <div className="empty-footer-div"></div>
       <div className="footer-links">
-        <a className="footer-link" href="https://github.com/nsemmler" target="_blank"><i class="fab fa-2x fa-github"></i></a>
-        <a className="footer-link" href="https://www.linkedin.com/in/natesemmler/" target="_blank"><i class="fab fa-2x fa-linkedin"></i></a>
+        <a className="footer-link" href="https://github.com/nsemmler" target="_blank"><i className="fab fa-2x fa-github"></i></a>
+        <a className="footer-link" href="https://www.linkedin.com/in/natesemmler/" target="_blank"><i className="fab fa-2x fa-linkedin"></i></a>
       </div>
       <div className="copyright-div">
         <p className="copyright">Ⓒ 2018 Nate Semmler</p>
