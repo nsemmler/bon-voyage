210ca318ae62a69960efcdd10438cad4121f399e
Nate <ndsunc@gmail.com>
2018-10-20 14:29:55 -0700

    Updated favorite button so it toggles the favorited heart icon when clicked

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,32 +1,66 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react'
 import MaterialIcon from 'material-icons-react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 import '../styling/CountryInfo.css'
 
-function GeneralInfo (props) {
-  return (
-    <div className="general-info-container">
-      <div className="info-div">
-        <h4 className="country-name">{ props.country.name }</h4>
-        <h5 className="country-capital">{ props.country.capital }</h5>
-        <ul className="info-items">
-          <li className="countryInfoItem">Population: { props.country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</li>
-          <li className="countryInfoItem">Currency: { props.country.currency_name } ({ props.country.currency_symbol })</li>
-          <li className="countryInfoItem">Languages: { JSON.parse(props.country.languages).join(', ') }</li>
-        </ul>
-        <button className="fav-btn" onClick={ () => props.updateUserFavorites(props.country, props.country.id) }><MaterialIcon icon={ (props.favorites.map(country => country.id).includes(props.country.id)) ? "favorite" : "favorite_border" } size="medium" color="#d10808"/></button>
-      </div>
-      <div className="flag-div">
-        <img src={ props.country.flag } alt={ `${props.country.name} Flag` } className="countryflag" />
-      </div>
-    </div>
-  )
-}
+class GeneralInfo extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isFavorited: false
+    }
+  }
 
-GeneralInfo.propTypes = {
-  country: PropTypes.object.isRequired,
-  updateUserFavorites: PropTypes.func.isRequired,
-  favorites: PropTypes.array.isRequired
+  componentWillMount () {
+    if (this.props.favorites.map(country => country.id).includes(this.props.country.id)) {
+      this.setState({ isFavorited: true })
+    } else {
+      this.setState({ isFavorited: false })
+    }
+  }
+
+  favoriteCountry () {
+    this.setState({ isFavorited: !this.state.isFavorited })
+  }
+
+  render () {
+    return (
+      <div className="general-info-container">
+        <div className="info-div">
+          <h4 className="country-name">{ this.props.country.name }</h4>
+          <h5 className="country-capital">{ this.props.country.capital }</h5>
+          <ul className="info-items">
+            <li className="countryInfoItem">Population: { this.props.country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }</li>
+            <li className="countryInfoItem">Currency: { this.props.country.currency_name } ({ this.props.country.currency_symbol })</li>
+            <li className="countryInfoItem">Languages: { JSON.parse(this.props.country.languages).join(', ') }</li>
+          </ul>
+          {
+            this.state.isFavorited &&
+            <button className="fav-btn" onClick={ () => {
+              this.favoriteCountry()
+              this.props.updateUserFavorites(this.props.country, this.props.country.id)
+            }}>
+              <MaterialIcon icon="favorite"  size="medium" color="#d10808"/>
+            </button>
+          }
+          {
+            !this.state.isFavorited &&
+            <button className="fav-btn" onClick={ () => {
+              this.favoriteCountry()
+              this.props.updateUserFavorites(this.props.country, this.props.country.id)
+            }}>
+              <MaterialIcon icon="favorite_border"  size="medium" color="#d10808"/>
+            </button>
+          }
+        </div>
+        <div className="flag-div">
+          <img src={ this.props.country.flag } alt={ `${this.props.country.name} Flag` } className="countryflag" />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default GeneralInfo
